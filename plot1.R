0bdbebfd14c9e75b7bedfae17f93b356fdea0995
Dearden <william.dearden@abbott.com>
2017-03-23 18:01:46 -0500

    plot1 created

--- plot1.R
+++ plot1.R
@@ -2,13 +2,21 @@ require(data.table)
 require(magrittr)
 require(lubridate)
 
+##Download file and save to table
 download.file(url='https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', destfile='power.zip')
 unzip('power.zip')
 power <- read.table('household_power_consumption.txt', header=TRUE, sep=';')
+
+##Subset by date and convert to kilowatts
 power$Date %<>% dmy
 range <- interval(ymd(20070201), ymd(20070202))
 power <- power[power$Date %within% range,]
 power$Global_active_power %<>% as.numeric %>% divide_by(1000)
-hist(power$Global_active_power[power$Global_active_power<=4],
-     xlab='Global Active Power (kilowatts)',
-     breaks=seq(0,6,0.5))
+
+##Generate plot
+hist(power$Global_active_power, main="Global Active Power", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+##Save to file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
