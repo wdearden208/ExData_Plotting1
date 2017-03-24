9fd6e8e0c628832197b3591dd54bbcc716ec77d9
Dearden <william.dearden@abbott.com>
2017-03-24 09:58:38 -0500

    add plot 4

--- plot3.R
+++ plot3.R
@@ -1,3 +1,10 @@
 #Run download and clean script and get cleaned power data
 source('get_power_data.R')
 power <- get_power_data()
+
+png("plot3.png", width=480, height=480)
+plot(power$Datetime, power$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+lines(power$Datetime, power$Sub_metering_2, type="l", col="red")
+lines(power$Datetime, power$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,17 @@
+#Run download and clean script and get cleaned power data
+source('get_power_data.R')
+power <- get_power_data()
+
+##Create plot
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(power$Datetime, power$Global_active_power, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(power$Datetime, power$Voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(power$Datetime, power$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+lines(power$Datetime, power$Sub_metering_2, type="l", col="red")
+lines(power$Datetime, power$Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(power$Datetime, power$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
