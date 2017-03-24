92f7aea53f63003b3403ff9affdec36c36a25e02
Dearden <william.dearden@abbott.com>
2017-03-24 10:06:45 -0500

    minor fix

--- plot4.R
+++ plot4.R
@@ -11,7 +11,7 @@ plot(power$Datetime, power$Voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(power$Datetime, power$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
 lines(power$Datetime, power$Sub_metering_2, type="l", col="red")
 lines(power$Datetime, power$Sub_metering_3, type="l", col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="n")
 plot(power$Datetime, power$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
