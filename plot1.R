1110dbb38f81eaab8d5f9f5e461248f4d523bdbe
Dearden <william.dearden@abbott.com>
2017-03-24 10:13:15 -0500

    minor fix

--- plot1.R
+++ plot1.R
@@ -3,9 +3,9 @@ source('get_power_data.R')
 power <- get_power_data()
 
 ##Generate plot
+png("plot1.png", width=480, height=480)
 hist(power$Global_active_power, main="Global Active Power", 
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 ##Save to file
-dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
--- plot2.R
+++ plot2.R
@@ -3,9 +3,9 @@ source('get_power_data.R')
 power <- get_power_data()
 
 ##Generate plot
+png("plot2.png", width=480, height=480)
 plot(power$Datetime, power$Global_active_power, type='l',
      xlab="", ylab="Global active power (kilowatts)")
 
 ##Save to file
-dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
