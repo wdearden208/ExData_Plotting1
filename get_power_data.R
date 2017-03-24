30a2815f7c2f89fc1b60f54f5d229557c401b5bc
Dearden <william.dearden@abbott.com>
2017-03-23 19:18:53 -0500

    split up and improve scripts

---
+++ get_power_data.R
@@ -0,0 +1,20 @@
+get_power_data <- function() {
+    require(data.table)
+    require(magrittr)
+    require(lubridate)
+    require(tidyr)
+    
+    ##Download file and save to table
+    download.file(url='https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', destfile='power.zip')
+    unzip('power.zip')
+    power <- read.table('household_power_consumption.txt', header=TRUE, sep=';')
+    
+    ##Subset by date and convert to kilowatts
+    power %<>% unite(Datetime, Date, Time, sep=' ')
+    power$Datetime %<>% dmy_hms
+    range <- interval(ymd(20070201), ymd(20070202))
+    power <- power[power$Datetime %within% range,]
+    power$Global_active_power %<>% as.numeric %>% divide_by(1000)
+    
+    power
+}
--- plot1.R
+++ plot1.R
@@ -1,6 +1,5 @@
-require(data.table)
-require(magrittr)
-require(lubridate)
+source('get_power_data.R')
+power <- get_power_data()
 
 ##Download file and save to table
 download.file(url='https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', destfile='power.zip')
@@ -8,9 +7,10 @@ unzip('power.zip')
 power <- read.table('household_power_consumption.txt', header=TRUE, sep=';')
 
 ##Subset by date and convert to kilowatts
-power$Date %<>% dmy
+power %<>% unite(Datetime, Date, Time, sep=' ')
+power$Datetime %<>% dmy_hms
 range <- interval(ymd(20070201), ymd(20070202))
-power <- power[power$Date %within% range,]
+power <- power[power$Datetime %within% range,]
 power$Global_active_power %<>% as.numeric %>% divide_by(1000)
 
 ##Generate plot
